7573b358cfd7ad0f6ac3040e782fd82478e0b2e8
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-07-17 16:45:36 +0530

    Written try and Catch and imporved the code with status codes

--- Course-management-backend/app.js
+++ Course-management-backend/app.js
@@ -21,72 +21,156 @@ const Course = require("./Schema/courseSchema");
 // Student routes
 
 app.get("/students",async (req,res)=>{
-    const student = await Student.find({});
-    res.send(student);
+    try{
+        const student = await Student.find({});
+        res.status(200).send(student);
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
+    
 
 });
 
 app.get("/students/:id",async(req,res)=>{
-    const { id } = req.params;
-    const student = await Student.findById(id).populate("courses");
-    res.send(student);
+    try{
+        const { id } = req.params;
+        const student = await Student.findById(id).populate("courses");
+        if(!student){
+            return res.status(404).send("Student Not Found");
+        }
+        res.status(200).send(student);    
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 app.patch("/students/:id",async(req,res)=>{
-    const { id } = req.params;
-    const newData = req.body;
-    const student = await Student.findByIdAndUpdate(id,newData,{new : true});
-    res.send(student);
+    try{
+        const { id } = req.params;
+        const newData = req.body;
+        const student = await Student.findByIdAndUpdate(id,newData,{new : true});
+        if(!student){
+        return res.status(404).send("Student Not Found");
+        }
+        res.status(200).send(student);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
+    
 });
 
 app.post("/students",async (req,res)=>{
-    console.log(req.body);
-    const student =  new Student(req.body);
-    await student.save();
-    res.send(student);
+    try{
+        const student =  new Student(req.body);
+        await student.save();
+        res.status(201).send(student);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
+    
 });
 
 app.delete("/students/:id",async(req,res)=>{
-    const { id } = req.params;
-    const student = await Student.findByIdAndDelete(id);
-    res.send({message : "deleted",student});
-
+    try{
+        const { id } = req.params;
+        const student = await Student.findByIdAndDelete(id);
+        if(!student){
+        return res.status(404).send("Student Not Found");
+        }
+        res.status(200).send({message : "deleted",student});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 // Courses routes
 
 app.post("/courses", async(req,res)=>{
-    const course = new Course(req.body);
-    await course.save();
-    res.send(course); 
+    try{
+        const course = new Course(req.body);
+        await course.save();
+        res.status(201).send(course); 
+    } catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
+    
 });
 
 app.get("/courses" , async(req,res)=>{
-    const course = await Course.find({});
-    res.send(course);
+    try{
+        const course = await Course.find({});
+        res.status(200).send(course);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
+    
 });
 
 app.get("/courses/:id",async(req,res)=>{
-    const { id } = req.params;
-    const course = await Course.findById(id).populate("students");
-    res.send(course);
+    try{
+        const { id } = req.params;
+        const course = await Course.findById(id).populate("students");
+        if(!course){
+            return res.status(404).send("Course Not Found");
+        }
+        res.status(200).send(course);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 app.patch("/courses/:id",async(req,res)=>{
+    try{
     const { id } = req.params;
     const course = await Course.findByIdAndUpdate(id,req.body,{returnDocument : "after"});
-    res.send(course);
+    if(!course){
+        return res.status(404).send("Course Not Found");
+    }
+    res.status(200).send(course);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
+    
 });
 
 app.delete("/courses/:id" , async(req,res)=>{
+    try{
     const { id } = req.params;
     const course = await Course.findByIdAndDelete(id);
-    res.send({message : "deleted" ,course});
+    if(!course){
+        return res.status(404).send("Course Not Found");
+    }
+    res.status(200).send({message : "deleted" ,course});
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
+   
 });
 
 // Enroll student in course
 
 app.post("/students/:id1/courses/:id2",async(req,res)=>{
+    try{
     const { id1 } = req.params;
     const student = await Student.findById(id1);
     console.log(student);
@@ -112,6 +196,12 @@ app.post("/students/:id1/courses/:id2",async(req,res)=>{
     await student.save();
     await course.save();
 
-    res.send({ student,course });
+    res.status(200).send({ student,course });
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+    }
+    
 });
 
