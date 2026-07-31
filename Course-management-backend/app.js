aa1ac8cdaa72d1b2822bb1ccf29715e9b74d081a
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-07-31 15:46:24 +0530

    implementing auth codes

--- Course-management-backend/app.js
+++ Course-management-backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 const mongoose = require("mongoose");
@@ -8,9 +9,12 @@ main()
 .catch(err =>console.log("Error Happend",err));
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/Course');
+
 }
 
+const User = require("./Models/userSchema");
 const port = 9000;
+
 app.listen(port,()=>{
     console.log("Listening to the port.");
 });
@@ -24,7 +28,22 @@ app.use(session({
 
 const passport = require("passport");
 const localStrategy = require("passport-local");
-const User = require("./Models/userSchema");
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(async(username,password,done)=>{
+    const user = await User.findOne({username});
+    if(!user){
+        return done(null,false);
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+        return done(null,false);
+    }
+    return done(null,user);
+}));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 const studentRoutes = require("./routes/students");
 const courseRoutes = require("./routes/courses");
