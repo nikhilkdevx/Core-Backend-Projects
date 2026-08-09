8318e9c0c2bc06f3984f33143dfe22e39c96b8a6
Nikhil Kumar <nkdevx.tech@gmail.com>
2026-08-09 17:14:54 +0530

    Done authentication part in backend

--- BITS Backend/app.js
+++ BITS Backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 const port = 6000;
 app.listen(port,()=>{
@@ -17,20 +18,24 @@ main()
     console.log("connected to MongoDB"))
 .catch(err=>console.log(err));
 
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("./Models/user");
 const userValidation = require("./validators/userValidation");
+const loginValidation = require("./validators/loginValidation");
 const ExpressError = require("./utils/ExpressError");
 
 // Auth 
 app.post("/auth/register",async (req,res)=>{
     const result = userValidation.validate(req.body);
-    console.log(result);
     if(result.error){
         throw new ExpressError(400,result.error.message);
-        return;
     }
     const {name,email,password,role} = req.body;
+    const existingUser = await User.findOne({ email });
+    if(existingUser){
+        throw new ExpressError(409,"Email already Registered");
+    }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({
         name,
@@ -39,14 +44,43 @@ app.post("/auth/register",async (req,res)=>{
         role,
     });
     await user.save();
-    res.status(201).json({message : " User registered Successfully"});
+    const safeUser = {
+        id : user._id,
+        name : user.name,
+        email : user.email,
+        role : user.role
+    };
+    res.status(201).json({message : " User registered Successfully", user : safeUser});
     
 });
 
-
-
-
-
+app.post("/auth/login",async(req,res)=>{
+    const result = loginValidation.validate(req.body);
+    if(result.error){
+        throw new ExpressError(400,result.error.message);
+    }
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+        throw new ExpressError(401,"Invalid Email or Password");
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+        throw new ExpressError(401,"Invalid Email or Password");
+    }
+    const token = jwt.sign({
+        userId : user._id , role : user.role},
+        process.env.JWT_SECERT,
+        {expiresIn : "7d"}
+    );
+    const safeUser = {
+        id : user._id,
+        name : user.name,
+        email : user.email,
+        role : user.role
+    };
+    res.status(200).json({message : "Login Success", token, user : safeUser});
+});
 
 app.use((err,req,res,next)=>{
     const {statusCode = 500 , message = "Something Went Wrong"} = err;
---
+++ BITS Backend/middlewares/verifyJWT.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const ExpressError = require("../utils/ExpressError");
+
+const verifyJWT = (req,res,next) =>{
+    const authHeader = req.headers.authorization;
+    if(!authHeader || authHeader.startswith("Bearer ")){
+        throw new ExpressError("401","Login required");
+    }
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token,process.env.JWT_SECERT);
+    req.user = decoded;
+    next();
+}
+
+module.exports = verifyJWT;
